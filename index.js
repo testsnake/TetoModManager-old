cc44549ab12ef4f7c29ded29b91a8a96debfdd01
testsnake <testsnake12@gmail.com>
2023-04-19 14:53:55 -0700

    Basic Setup

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const { app, BrowserWindow } = require('electron')
+const fs = require('fs')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        titleBarStyle: 'hidden',
+        titleBarOverlay: {
+            color: '#2f3241',
+            symbolColor: '#FFFFFF',
+            height: 20
+        },
+        backgroundColor: '#191825',
+        minWidth: 600,
+        minHeight: 400,
+
     })
 
-    win.loadFile('index.html')
+    win.loadFile('src/index.html')
 }
 
 app.whenReady().then(() => {
